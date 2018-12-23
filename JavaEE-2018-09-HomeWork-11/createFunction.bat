ceb19df472f9ac8d7546a3053db67c12f3024ffa
VSkurikhin <stalker@quake.ru>
2018-12-24 01:23:07 +0300

    JavaEE-2018-09-HomeWork-11.

--- JavaEE-2018-09-HomeWork-11/createFunction.bat
+++ JavaEE-2018-09-HomeWork-11/createFunction.bat
@@ -3,7 +3,7 @@ REM Build:
 REM       mvn clean compile dependency:copy-dependencies
 SET CS="encoding=UTF-8"
 SET CPTARGET=HomeWorkPersistent\target
-SET CP=-cp %CP%\classes;%CP%\dependency\*;%CP%\*
+SET CP=-cp %CPTARGET%\classes;%CPTARGET%\dependency\*;%CPTARGET%\*
 SET JAVA_OPTS=%CP% -Dfile.%CS% -Dsun.stdout.%CS% -Dsun.err.%CS%
 CHCP 65001
 java %JAVA_OPTS% ru.otus.CreateFunction
